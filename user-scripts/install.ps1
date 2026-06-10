9266ad7c1c83d352b0389c1f3732d4bc8d56bb6d
damien-urruty-sonarsource <damien-urruty-sonarsource>
2026-06-10 14:48:56 +0000

    Prepare next development iteration

--- user-scripts/install.ps1
+++ user-scripts/install.ps1
@@ -49,7 +49,7 @@ function Add-ToUserPath {
 #Write-Host 'Fetching latest version...'
 #$SonarVersion = Resolve-LatestVersion
 
-$SonarVersion = "0.14.1.2338"
+$SonarVersion = "1.0.0.2628"
 Write-Host "Latest version: $SonarVersion"
 
 $Filename     = "sonarqube-cli-$SonarVersion-$Platform.exe"
